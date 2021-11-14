11ecb8711cf8d3dc603705eb5f8f1473110bfcfc
xin0415 <34253658+xin0415@users.noreply.github.com>
2021-11-13 20:11:22 -0500

    implement update

--- src/actions/posts.js
+++ src/actions/posts.js
@@ -3,17 +3,26 @@ import * as api from '../api';
 export const getPosts=()=> async (dispatch)=>{
     try{
         const{data} =await api.fetchPosts();
-        dispatch({type:'FETCH_ALL',payload:[]})
+        dispatch({type:'FETCH_ALL',payload:data});
     }catch(error){
-        console.log(error.message);
+        console.log(error);
     }
 }
 
 export const createPost =(post)=> async (dispatch)=>{
     try{
         const {data}=await api.createPost(post);
-        dispatch({type:'CREATE',payload:data})
+        dispatch({type:'CREATE',payload:data});
     }catch(error){
-        console.log(error.message);
+        console.log(error);
+    }
+}
+
+export const updatePost=(id, post)=>async (dispatch)=>{
+    try{
+        const {data}=await api.updatePost(id,post);
+        dispatch({type:'UPDATE',payload:data});
+    }catch(error){
+        console.log(error);
     }
 }
--- src/reducers/posts.js
+++ src/reducers/posts.js
@@ -1,10 +1,13 @@
-export default (posts=[], action)=>{
+const requestPost= (posts=[], action)=>{
     switch (action.type) {
         case 'FETCH_ALL':
             return action.payload;
         case 'CREATE':
             return [...posts,action.payload];
+        case 'UPDATE':
+            return posts.map((post)=>post._id===action.payload._id?action.payload:post);
         default:
             return posts;
     }
 }
+export default requestPost;
