8e83b48222c2eb8a52a5f7552b07d5360db42194
xin0415 <34253658+xin0415@users.noreply.github.com>
2022-09-13 00:44:22 -0400

    new component

--- src/App.js
+++ src/App.js
@@ -3,16 +3,22 @@ import { Container} from "@material-ui/core";
 import Navbar from "./components/Navbar/Navbar";
 import Home from "./components/Home/Home";
 import Auth from "./components/Auth/Auth";
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import PostDetails from "./components/PostDetails/PostDetails";
+import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom';
 
 const App=()=>{
     return (
         <BrowserRouter>
-            <Container maxidth="lg">
+            <Container maxidth="xl">
                 <Navbar />
                     <Routes>
-                        <Route path="/" exact element={<Home/>} />
-                        <Route path="/auth" exact element={<Auth/>}/>
+                        <Route path="/" exact component={()=><Navigate to="/posts"/>} />
+                        <Route path="/posts" exact component={Home}/>
+                        <Route path="/posts/search" exact component={Home}/>
+                        <Route path="/posts/:id" component={PostDetails}/>
+                        <Route path="/auth" exact component={Auth}/>
+                        {/* <Route path="/" exact element={<Home/>} />
+                        <Route path="/auth" exact element={<Auth/>}/> */}
                     </Routes>
             </Container>
         </BrowserRouter>
